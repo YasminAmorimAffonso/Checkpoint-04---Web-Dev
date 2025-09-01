7ab93711f3371849df5a975e09a00857bd421089
Yasmin Amorim Affonso <yasminamorimaffonso@gmail.com>
2025-09-01 18:52:46 -0300

    1900

--- checkpoint04/src/main.jsx
+++ checkpoint04/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './index.css'
 import Home from './Home.jsx'
 import Loja from './loja.jsx'
@@ -10,14 +10,14 @@ import Carrinho from './carrinho.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
 
-<HashRouter>
+<BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/loja" element={<Loja />} />
         <Route path="/produto" element={<Produto />} />
         <Route path="/carrinho" element={<Carrinho />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
 
   </StrictMode>,
 )
